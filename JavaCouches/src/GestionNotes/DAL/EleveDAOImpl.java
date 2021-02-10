d5968f8b0fef0ce652471719d7c6cdbaa5d47872
teobryer <teocalme@gmail.com>
2021-02-10 16:17:04 +0100

    retour de l'id généré lors de l'insertion

--- JavaCouches/src/GestionNotes/DAL/EleveDAOImpl.java
+++ JavaCouches/src/GestionNotes/DAL/EleveDAOImpl.java
@@ -3,6 +3,7 @@ package GestionNotes.DAL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +24,7 @@ public class EleveDAOImpl implements EleveDAO {
 	@Override
 	public void insert(Eleve eleve) throws EleveDALException {
 		try (Connection cnx = JdbcTools.getConnection()) {
-			PreparedStatement stmt = cnx.prepareStatement(INSERT);
+			PreparedStatement stmt = cnx.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
 			stmt.setString(1, eleve.getLastName());
 			stmt.setString(2, eleve.getFirstName());
 		
@@ -31,7 +32,13 @@ public class EleveDAOImpl implements EleveDAO {
 			stmt.setString(4, eleve.getClasse());
 		
 			stmt.executeUpdate();
-
+			
+			ResultSet rs = stmt.getGeneratedKeys();
+			int generatedKey = 0;
+			if (rs.next()) {
+			    generatedKey = rs.getInt(1);
+			}
+			System.out.println(generatedKey);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new EleveDALException("problème dans l'insertion d'un car");
@@ -58,6 +65,7 @@ public class EleveDAOImpl implements EleveDAO {
 				notesList,
 				rs.getString("classe"));
 				result.add(eleve);
+				
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
